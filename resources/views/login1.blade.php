9a05490ad6b68e05c467bd14e3abd1ba54af5dfe
Cobygr <cobygrinfo@gmail.com>
2025-02-13 01:47:32 +0900

    ユーザー登録機能のCSSの適用

--- resources/views/login1.blade.php
+++ resources/views/login1.blade.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/scss/app.scss','resources/css/app.css','resources/js/app.js'])
-  </head>
+    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
+</head>
+
 <body>
-  ログイン画面
-  <div class="container">
-    <h2>Hello sass </h2>
-  </div>
+    ログイン画面
+    <div class="container">
+        <h2>Hello sass </h2>
+    </div>
 </body>
+
 </html>
--- resources/views/register1.blade.php
+++ resources/views/register1.blade.php
@@ -1,21 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
+<html lang="ja">
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-  </head>
+    @vite(['resources/scss/style.scss','resources/js/app.js'])
+</head>
+
 <body>
-  <form action="{{ route('register.store') }}" method="POST">
-    @csrf
-    <label for="email">メールアドレス</label>
-    <input type="email" name="email" id="email" required>
-    <br>
-    <label for="password">パスワード</label>
-    <input type="password" name="password" id="password" required>
-    <br>
-    <button type="submit">登録</button>
-  </form>
+    <header class="l-header">
+      <div class="l-header__logo">
+        <a href="#">WEBKATU MARKET</a>
+    </div>    
+        <nav class="l-header__nav">
+            <a href="#" class="l-header__nav__item">ログイン</a>
+            <a href="#" class="l-header__nav__item">ユーザー登録</a>
+        </nav>
+    </header>
+    <div class="l-register">
+      <h1 class="l-register__title">ユーザー登録</h1>
+        <form action="{{ route('register.store') }}" method="POST">
+            @csrf
+            <label for="email">メールアドレス</label>
+            <input type="email" name="email" id="email" required>
+            <br>
+            <label for="password">パスワード</label>
+            <input type="password" name="password" id="password" required>
+            <br>
+            <button type="submit">登録</button>
+        </form>
+    </div>
 </body>
+
 </html>
