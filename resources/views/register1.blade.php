e7a1480b6702ff77b453706c025aae4ab1482c43
Cobygr <cobygrinfo@gmail.com>
2025-02-18 07:48:54 +0900

    登録ページの実装（CSSの適用）

--- resources/views/register1.blade.php
+++ resources/views/register1.blade.php
@@ -6,30 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/scss/style.scss','resources/js/app.js'])
+    @vite(['resources/scss/style.scss', 'resources/js/app.js'])
 </head>
 
 <body>
     <header class="l-header">
-      <div class="l-header__logo">
-        <a href="#">WEBKATU MARKET</a>
-    </div>    
+        <div class="l-header__logo">
+            <a href="#">WEBKATU MARKET</a>
+        </div>
         <nav class="l-header__nav">
             <a href="#" class="l-header__nav__item">ログイン</a>
             <a href="#" class="l-header__nav__item">ユーザー登録</a>
         </nav>
     </header>
     <div class="l-register">
-      <h1 class="l-register__title">ユーザー登録</h1>
         <form action="{{ route('register.store') }}" method="POST">
+            <h2 class="l-register__title">ユーザー登録</h2>
             @csrf
-            <label for="email">メールアドレス</label>
+            <label for="email">Email</label>
             <input type="email" name="email" id="email" required>
             <br>
             <label for="password">パスワード</label>
             <input type="password" name="password" id="password" required>
             <br>
-            <button type="submit">登録</button>
+            <label for="password">パスワード（再入力）</label>
+            <input type="password" name="pass_re" id="pass_re" required>
+            <br>
+            <!--  ボタンを右端に寄せるためのコンテナ -->
+            <div class="l-register__btn">
+                <button type="submit">登録</button>
+            </div>
+
         </form>
     </div>
 </body>
